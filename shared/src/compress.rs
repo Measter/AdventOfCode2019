6933692bb4dba41ab193edabc1eb222ff72b0534
Stuart Haidon <serayen.sh@gmail.com>
2021-02-03 22:51:19 +0000

    Implement 3-stage compression.

--- shared/src/compress.rs
+++ shared/src/compress.rs
@@ -1,21 +1,33 @@
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    convert::TryInto,
-    io::Write,
-};
+use std::{collections::BTreeMap, convert::TryInto, io::Write};
 
 use crate::{
     RunLengthEncoded, ADDR_SIZE, DICT_START_ADDR, LOOKUP_START, NUM_RECORD_ADDR, RECORD_START_ADDR,
 };
 
 const MAX_DICT_LEN: usize = 512;
-const MAX_DICT_ENTRY_LEN: usize = 5;
+const MAX_DICT_ENTRY_LEN: usize = 10;
 
+// Applies a 3-stage compression:
+//
+// * Stage 1
+//   Uses an unrestricted dictionary.
+//   Only reads and stores the inputs, doesn't compress them.
+//
+// * Stage 2
+//   Re-uses the stage 1 dictionary to count how many times each sequence
+//   is found. Input is compressed at this stage.
+//
+// * Stage 3
+//   Uses the counts from stage 2 to build a dictionary consisting of the
+//   byte sequences seen.
+//   Then re-maps the compressed records from stage 2 to the new dictionary.
 #[derive(Debug)]
 pub struct Compress {
-    dict: BTreeMap<Vec<u8>, u16>,
-    records: Vec<Vec<u16>>,
-    max_record_len: u16,
+    // Stage 0 dictionary.
+    dict: BTreeMap<Vec<u8>, u32>,
+    // Uncompressed inputs.
+    records: Vec<Vec<u8>>,
+    max_record_len: u32,
 }
 
 impl Default for Compress {
@@ -31,80 +43,137 @@ impl Compress {
     #[must_use]
     pub fn new() -> Self {
         Self {
-            dict: (0..=255_u16).map(|b| (vec![b as u8], b as _)).collect(),
-            records: Vec::new(),
-            max_record_len: 0,
-        }
-    }
-
-    #[must_use]
-    pub fn with_dict(dict: BTreeSet<u8>) -> Self {
-        Self {
-            dict: dict
-                .into_iter()
-                .zip(0..)
-                .map(|(c, i)| (vec![c], i))
-                .collect(),
+            dict: (0..=255_u32).map(|b| (vec![b as u8], b as _)).collect(),
             records: Vec::new(),
             max_record_len: 0,
         }
     }
 
     pub fn add_record<T: AsRef<[u8]>>(&mut self, record: T) {
+        // Applies stage 1 of the process above.
+        // We only iterate over the input, not compress it here.
         let record = record.as_ref();
 
-        let mut compressed = Vec::new();
-        let mut cur_seq: &[u8] = &[];
         let mut cur_seq_start = 0;
 
         for idx in 0..record.len() {
             let new_seq = &record[cur_seq_start..=idx];
 
-            if self.dict.contains_key(new_seq) && new_seq.len() <= MAX_DICT_ENTRY_LEN {
-                cur_seq = new_seq;
-            } else {
-                // Write current sequence to output.
-                compressed.push(self.dict[cur_seq]);
-
-                if self.dict.len() < MAX_DICT_LEN {
-                    self.dict.insert(
+            if !self.dict.contains_key(new_seq) || new_seq.len() >= MAX_DICT_ENTRY_LEN {
+                // A bit odd, but we don't want to remove the previous entry.
+                if !self.dict.contains_key(new_seq) {
+                    let val = self.dict.insert(
                         new_seq.to_vec(),
                         self.dict
                             .len()
                             .try_into()
-                            .expect("Unable to encode length as u16"),
+                            .expect("Unable to encode length as u32"),
                     );
+
+                    if val.is_some() {
+                        panic!("Incorrectly removed previous entry");
+                    }
                 }
 
                 // Restart the sequence starting with the current byte.
                 cur_seq_start = idx;
-                cur_seq = &record[idx..=idx];
             }
         }
 
-        // We may have left-over data in cur_seq, but we'll already have
-        // seen it.
-        if !cur_seq.is_empty() {
-            compressed.push(self.dict[cur_seq]);
-        }
-
-        self.records.push(compressed);
         self.max_record_len = self
             .max_record_len
             .max(record.len().try_into().expect("Length too long"));
+
+        // Finally store the input for compression later.
+        self.records.push(record.to_owned());
+    }
+
+    fn apply_stage2(&self) -> (BTreeMap<u32, u32>, Vec<Vec<u32>>) {
+        // Iterates over the inputs a second time, counting how many times each
+        // sequences was seen while we compress.
+        // This is, essentially, re-applying what we did in stage 1, but without modifying
+        // the dictionary.
+
+        let mut counts = BTreeMap::new();
+        let mut compressed = Vec::new();
+
+        for record in &self.records {
+            let mut cur_compressed = Vec::new();
+            let mut cur_seq: &[u8] = &[];
+            let mut cur_seq_start = 0;
+
+            for idx in 0..record.len() {
+                let new_seq = &record[cur_seq_start..=idx];
+
+                if self.dict.contains_key(new_seq) {
+                    cur_seq = new_seq;
+                } else {
+                    cur_compressed.push(self.dict[cur_seq]);
+                    *counts.entry(self.dict[cur_seq]).or_default() += 1;
+
+                    // Restart the sequence from the current byte.
+                    cur_seq_start = idx;
+                    cur_seq = &record[idx..=idx];
+                }
+            }
+
+            // We make have left-over data, but it's already been seen.
+            if !cur_seq.is_empty() {
+                cur_compressed.push(self.dict[cur_seq]);
+                *counts.entry(self.dict[cur_seq]).or_default() += 1;
+            }
+
+            compressed.push(cur_compressed);
+        }
+
+        (counts, compressed)
+    }
+
+    fn apply_stage3(
+        &self,
+        counts: BTreeMap<u32, u32>,
+        compressed: Vec<Vec<u32>>,
+    ) -> (BTreeMap<Vec<u8>, u16>, Vec<Vec<u16>>) {
+        // Here we build up the final dictionary and reprocess the compressed entries.
+        // We start by building the final dictionary. While we do that, we build a map
+        // between the stage 1 dictionary and the final dictionary.
+
+        let id_to_seq: BTreeMap<_, _> = self.dict.iter().map(|(a, b)| (b, a)).collect();
+        let mut id_to_new_id: BTreeMap<u32, u16> = BTreeMap::new();
+        let mut final_dict: BTreeMap<Vec<u8>, u16> = BTreeMap::new();
+
+        let single_bytes_seen = counts.into_iter().filter(|(_, times_seen)| *times_seen > 0);
+        for (id, _) in single_bytes_seen {
+            let new_id: u16 = final_dict
+                .len()
+                .try_into()
+                .expect("Unable to encode length as u16");
+            final_dict.insert(id_to_seq[&id].to_vec(), new_id);
+            id_to_new_id.insert(id, new_id);
+        }
+
+        // Now we need to re-map the compressed records to the new IDs.
+        let final_compressed = compressed
+            .into_iter()
+            .map(|record| record.into_iter().map(|id| id_to_new_id[&id]).collect())
+            .collect();
+
+        (final_dict, final_compressed)
     }
 
     /// Stores the compressed archive into a data structure readable by `Decompress`.
     #[must_use]
     pub fn store_archive(&self) -> Vec<u8> {
+        let (stage2_counts, compressed) = self.apply_stage2();
+        let (final_dict, compressed_records) = self.apply_stage3(stage2_counts, compressed);
+
         let mut archive = Vec::new();
-        let records_len: u16 = self
-            .records
+        let records_len: u16 = compressed_records
             .len()
             .try_into()
             .expect("Records length too long");
 
-        let dictionary_keys: BTreeMap<_, _> = self.dict.iter().map(|(k, v)| (v, k)).collect();
+        let dictionary_keys: BTreeMap<_, _> = final_dict.iter().map(|(k, v)| (v, k)).collect();
         let lookup_length_bytes = dictionary_keys.len() * ADDR_SIZE;
         archive.resize(lookup_length_bytes + LOOKUP_START, 0);
 
@@ -141,7 +210,7 @@ impl Compress {
         let record_start_addr: u16 = archive.len().try_into().expect("Record length too long");
         archive[RECORD_START_ADDR].copy_from_slice(record_start_addr.to_le_bytes().as_ref());
 
-        for record in &self.records {
+        for record in compressed_records {
             // Write the length of the record.
             let cur_record_len = record.len().try_into().expect("Record len too big");
             archive
@@ -149,7 +218,7 @@ impl Compress {
                 .unwrap();
 
             // Write the record.
-            for &val in record {
+            for val in record {
                 archive
                     .write_all(RunLengthEncoded::encode(val).as_ref())
                     .unwrap();
@@ -162,18 +231,86 @@ impl Compress {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+
+    #[test]
+    fn stage1() {
+        let input = "Hello World!";
+
+        let mut archive = Compress::new();
+        archive.add_record(input);
+
+        let expected_record = vec![input.as_bytes().to_vec()];
+        let mut expected_dict: BTreeMap<_, _> = (0..=255_u8).map(|b| (vec![b], b.into())).collect();
+
+        let len_dict: u32 = expected_dict.len().try_into().unwrap();
+        expected_dict.extend(
+            input
+                .as_bytes()
+                .windows(2)
+                .zip(len_dict..)
+                .map(|(bytes, id)| (bytes.to_vec(), id)),
+        );
+
+        assert_eq!(expected_record, archive.records);
+        assert_eq!(expected_dict, archive.dict);
+    }
+
+    #[test]
+    fn stage2() {
+        let input = "Hello World!";
+
+        let mut archive = Compress::new();
+        archive.add_record(input);
+
+        let (actual_stage2, stage2_compressed) = archive.apply_stage2();
+
+        let mut expected_counts = BTreeMap::new();
+        expected_counts.extend((256..).step_by(2).map(|id| (id, 1)).take(6));
+        assert_eq!(expected_counts, actual_stage2);
+
+        let expected_compressed: Vec<Vec<u32>> = vec![vec![256, 258, 260, 262, 264, 266]];
+        assert_eq!(expected_compressed, stage2_compressed);
+    }
+
+    #[test]
+    fn stage3() {
+        let input = "Hello World!";
+
+        let mut archive = Compress::new();
+        archive.add_record(input);
+
+        let (stage2_count, stage2_compressed) = archive.apply_stage2();
+        let (stage3_dict, compressed) = archive.apply_stage3(stage2_count, stage2_compressed);
+
+        let expected_dict: BTreeMap<_, _> = [
+            (vec![72, 101], 0),
+            (vec![87, 111], 3),
+            (vec![100, 33], 5),
+            (vec![108, 108], 1),
+            (vec![111, 32], 2),
+            (vec![114, 108], 4),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+        let expected_compressed: Vec<Vec<u16>> = vec![vec![0, 1, 2, 3, 4, 5]];
+
+        assert_eq!(expected_dict, stage3_dict);
+        assert_eq!(expected_compressed, compressed);
+    }
 
     #[test]
     fn compress_single_record() {
         let mut archive = Compress::new();
         archive.add_record("TOBEORNOTTOBEORTOBEORNOT");
+        let (stage2_counts, stage2_compressed) = archive.apply_stage2();
+        let (_, records) = archive.apply_stage3(stage2_counts, stage2_compressed);
 
-        let expected = vec![vec![
-            84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263,
-        ]];
+        let expected = vec![vec![5, 2, 3, 4, 1, 6, 5, 2, 3, 0]];
 
-        assert_eq!(archive.records, expected);
+        assert_eq!(records, expected);
     }
 
     #[test]
@@ -181,21 +318,18 @@ mod tests {
         let mut archive = Compress::new();
         archive.add_record("TOBEORNOTTOBEORTOBEORNOT");
         archive.add_record("TOBEORNOTTOBEORTOBEORNOT");
+        let (stage2_counts, stage2_compressed) = archive.apply_stage2();
+        let (_, records) = archive.apply_stage3(stage2_counts, stage2_compressed);
 
-        let expected = vec![
-            vec![
-                84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263,
-            ],
-            vec![268, 260, 262, 264, 257, 269, 271, 270, 84],
-        ];
+        let expected = vec![vec![1, 0, 1, 1, 0], vec![1, 0, 1, 1, 0]];
 
-        assert_eq!(archive.records, expected);
+        assert_eq!(records, expected);
     }
 
     #[test]
     fn archive_round_trip() {
         let input_text = "Hello World!";
-        let mut archive = Compress::with_dict(input_text.bytes().collect());
+        let mut archive = Compress::new();
 
         archive.add_record(input_text);
         archive.add_record(input_text);
@@ -215,12 +349,7 @@ mod tests {
     #[test]
     fn big_file_test() {
         let input_text = std::fs::read_to_string("test_data/aoc_2002.txt").unwrap();
-        let dict: BTreeSet<_> = input_text
-            .lines()
-            .flat_map(str::as_bytes)
-            .copied()
-            .collect();
-        let mut archive = Compress::with_dict(dict);
+        let mut archive = Compress::new();
 
         let mut num_records = 0;
         for line in input_text.lines() {
