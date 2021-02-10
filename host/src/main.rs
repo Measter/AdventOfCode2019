686e80c04ca1cdb295eb0bfb3af65c52c9612ba4
Stuart Haidon <serayen.sh@gmail.com>
2021-02-10 07:16:04 +0000

    Include number of records in uncompressed output.

--- host/src/main.rs
+++ host/src/main.rs
@@ -3,13 +3,13 @@ use std::{
     io::{ErrorKind, Write},
 };
 
-use shared::compress::Compress;
+use shared::{compress::Compress, RunLengthEncoded};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn archive_input<F>(day: u8, dict_preload: &[&str], record_func: F) -> Result<()>
 where
-    F: Fn(&mut Compress, &str) -> usize,
+    F: Fn(&mut Compress, &str) -> (u16, usize),
 {
     print!("Day {}... ", day);
 
@@ -27,7 +27,7 @@ where
         .iter()
         .for_each(|d| archive.add_dictionary_entry(d));
 
-    let max_len = record_func(&mut archive, &contents);
+    let (num_records, max_len) = record_func(&mut archive, &contents);
     let compressed = archive.store_archive();
 
     let mut output_file = File::create(format!("../inputs/aoc_19{:02}.bin", day))?;
@@ -35,6 +35,7 @@ where
     if compressed.len() > contents.len() {
         println!("Not compressed.");
         output_file.write_all(&[0])?;
+        output_file.write_all(RunLengthEncoded::encode(num_records).as_ref())?;
         output_file.write_all(contents.as_bytes())?;
     } else {
         println!("Compressed. Max record: {}b", max_len);
@@ -45,10 +46,10 @@ where
     Ok(())
 }
 
-fn day1(archive: &mut Compress, input: &str) -> usize {
-    let max_len = input.lines().fold(0, |max_len, record| {
+fn day1(archive: &mut Compress, input: &str) -> (u16, usize) {
+    let max_len = input.lines().fold((0, 0), |(count, max_len), record| {
         archive.add_record(record);
-        max_len.max(record.len())
+        (count + 1, max_len.max(record.len()))
     });
 
     max_len
