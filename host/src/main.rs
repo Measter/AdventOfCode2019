711d89887c200e5312fa74e495e24bb1348547c0
Stuart Haidon <serayen.sh@gmail.com>
2021-02-13 12:13:52 +0000

    Use Shared::Writer to process files.

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,15 +1,15 @@
 use std::{
     fs::{read_to_string, File},
-    io::{ErrorKind, Write},
+    io::ErrorKind,
 };
 
-use shared::{compress::Compress, RunLengthEncoded};
+use shared::Writer;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn archive_input<F>(day: u8, dict_preload: &[&str], record_func: F) -> Result<()>
 where
-    F: Fn(&mut Compress, &str) -> (u16, usize),
+    F: for<'a> Fn(&mut Writer<'a>, &'a str) -> usize,
 {
     print!("Day {}... ", day);
 
@@ -22,41 +22,29 @@ where
         Err(e) => return Err(e.into()),
     };
 
-    let mut archive = Compress::new();
-    dict_preload
-        .iter()
-        .for_each(|d| archive.add_dictionary_entry(d));
+    let mut archive = Writer::new();
+    archive.preload_dict(dict_preload);
+    let max_len = record_func(&mut archive, &contents);
 
-    let (num_records, max_len) = record_func(&mut archive, &contents);
-    let compressed = archive.store_archive();
+    let output_file = File::create(format!("../inputs/aoc_19{:02}.bin", day))?;
+    archive.write(output_file).unwrap();
 
-    let mut output_file = File::create(format!("../inputs/aoc_19{:02}.bin", day))?;
-
-    if compressed.len() > contents.len() {
-        println!("Not compressed.");
-        output_file.write_all(&[0])?;
-        output_file.write_all(RunLengthEncoded::encode(num_records).as_ref())?;
-        output_file.write_all(contents.as_bytes())?;
-    } else {
-        println!("Compressed. Max record: {}b", max_len);
-        output_file.write_all(&[1])?;
-        output_file.write_all(&compressed)?;
-    }
+    println!(" Written. Longest record: {}b", max_len);
 
     Ok(())
 }
 
-fn day1(archive: &mut Compress, input: &str) -> (u16, usize) {
-    let max_len = input.lines().fold((0, 0), |(count, max_len), record| {
+fn by_line<'a>(archive: &mut Writer<'a>, input: &'a str) -> usize {
+    let max_len = input.lines().fold(0, |max_len, record| {
         archive.add_record(record);
-        (count + 1, max_len.max(record.len()))
+        max_len.max(record.len())
     });
 
     max_len
 }
 
 fn main() -> Result<()> {
-    archive_input(1, &[], day1)?;
+    archive_input(1, &[], by_line)?;
 
     Ok(())
 }
